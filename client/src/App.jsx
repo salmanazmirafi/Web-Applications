6ae6afe7b0e4e1430440072cea52744a6c578ed9
Salman Azmi Rafi <116106498+salmanazmirafi@users.noreply.github.com>
2022-12-19 22:06:52 +0600

    Revert "Update"
    This reverts commit e2109c1ef20153a5854ecdfec26586e1ea1649e8.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,10 @@
-import Cart from "./pages/Cart";
 import Home from "./pages/home/Home";
-import Login from "./pages/Login";
-import Product from "./pages/Product";
-import ProductList from "./pages/ProductList";
-import Register from "./pages/Register";
 
 const App = () => {
   return (
     <>
      
       <Home/> 
-      {/* <ProductList/> */}
-      {/* <Product/> */}
-      {/* <Cart/> */}
-      {/* <Login/> */}
-      {/* <Register/> */}
     </>
   )
 };
