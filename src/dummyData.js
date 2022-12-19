b45779f37a26f9cd4bc08305399332e9f6e32264
salmanazmirafi <116106498+salmanazmirafi@users.noreply.github.com>
2022-12-20 03:45:39 +0600

    Update

--- src/dummyData.js
+++ src/dummyData.js
@@ -9,11 +9,11 @@ export const userData = [
     },
     {
       name: "Mar",
-      "Active User": 5000,
+      "Active User": 4000,
     },
     {
       name: "Apr",
-      "Active User": 4000,
+      "Active User": 5000,
     },
     {
       name: "May",
@@ -45,7 +45,7 @@ export const userData = [
     },
     {
       name: "Dec",
-      "Active User": 3000,
+      "Active User": 1000,
     },
   ];
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import Chart from '../../components/chart/Chart'
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo'
+import WidgetLg from '../../components/widgetLg/WidgetLg'
+import WidgetSm from '../../components/widgetSm/WidgetSm'
+import { userData } from '../../dummyData'
 import './home.css'
 
 const Home = () => {
   return (
     <div className='home'>
       <FeaturedInfo/>
+      <Chart data={userData} title="User Analytics" grid dataKey="Active User" />
+      <div className="homeWidgets">
+        <WidgetSm/>
+        <WidgetLg/>
+      </div>
     </div>
   )
 }
