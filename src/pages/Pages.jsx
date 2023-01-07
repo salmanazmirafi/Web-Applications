7faa7d0d8f423282f76af315866622b25f62b54a
salmanazmirafi <116106498+salmanazmirafi@users.noreply.github.com>
2023-01-07 19:23:08 +0600

    Update

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,12 +1,25 @@
 import React from "react";
+import Dcard from "../components/annocument/Annocument";
 import Home from "../components/categories/Home";
+import Discount from "../components/discount/Discount";
 import FlashDeals from "../components/flashDeals/FlashDeals";
+import NewArrivals from "../components/newarrivals/NewArrivals";
+import Shop from "../components/shops/Shop";
+import TopCate from "../components/top/TopCate";
+import Wrapper from "../components/wrapper/Wrapper";
 
 const Pages = ({ productItems, addToCart, CartItem, shopItems }) => {
+  console.log(shopItems);
   return (
     <div>
       <Home CartItem={CartItem} />
       <FlashDeals productItems={productItems} addToCart={addToCart} />
+      <TopCate />
+      <NewArrivals />
+      <Discount />
+      <Shop shopItems={shopItems} addToCart={addToCart} />
+      <Dcard />
+      <Wrapper />
     </div>
   );
 };
